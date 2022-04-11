0b7c9b6eae61483f6672712d1d947169f3ae5a96
mintmintra <mintracss019@gmail.com>
2022-04-11 11:56:41 +0700

    custom server

--- server/pages/index.js
+++ server/pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Layout from "../components/Layout";
 import axios from 'axios';
+import Link from "next/link";
 
 class Index extends Component{
 
@@ -16,7 +17,9 @@ class Index extends Component{
                     <div key={blog.id} className="col-6">
                         <img src={blog.thumbnail} className="img-fluid" />
                         <h4 className="mt-3">
-                            {blog.subject}
+                            <Link as={'/blog/detail/' + blog.id} href={`/blog/detail?id=${blog.id}`}>
+                                {blog.subject}
+                            </Link>
                         </h4>
                         <hr />
                         <p className="text-justify">{ blog.intro}</p>
